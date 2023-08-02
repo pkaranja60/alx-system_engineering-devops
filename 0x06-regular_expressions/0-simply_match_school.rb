b4572e9ccea948b59835dffda24b431b611861c3
Peter karanja <peterkaranja60@gmail.com>
2023-08-03 00:29:53 +0300

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-if ARGV.length != 1
-    puts "Usage: #{$PROGRAM_NAME} <string>"
-    exit(1)
-  end
-  
-  input_string = ARGV[0]
-  
-  regex = Oniguruma::ORegexp.new("School")
-  
-  if regex.match(input_string)
-    puts "Match found: #{input_string}"
-  else
-    puts "No match found"
-  end
+puts ARGV[0].scan(/School/).join
