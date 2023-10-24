7ef238bcf6aa8cddc017c61797ed795f2d4c7084
Peter karanja <peterkaranja60@gmail.com>
2023-10-24 12:50:26 +0300

    UPDATE

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,18 +11,17 @@ from sys import argv
 
 
 if __name__ == "__main__":
-
     sessionReq = requests.Session()
 
     idEmp = argv[1]
-    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
-    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+    idURL = "https://jsonplaceholder.typicode.com/users/{}/todos".format(idEmp)
+    nameURL = "https://jsonplaceholder.typicode.com/users/{}".format(idEmp)
 
     employee = sessionReq.get(idURL)
     employeeName = sessionReq.get(nameURL)
 
     json_req = employee.json()
-    usr = employeeName.json()['username']
+    usr = employeeName.json()["username"]
 
     totalTasks = []
     updateUser = {}
@@ -30,12 +29,13 @@ if __name__ == "__main__":
     for all_Emp in json_req:
         totalTasks.append(
             {
-                "task": all_Emp.get('title'),
-                "completed": all_Emp.get('completed'),
+                "task": all_Emp.get("title"),
+                "completed": all_Emp.get("completed"),
                 "username": usr,
-            })
+            }
+        )
     updateUser[idEmp] = totalTasks
 
     file_Json = idEmp + ".json"
-    with open(file_Json, 'w') as f:
+    with open(file_Json, "w") as f:
         json.dump(updateUser, f)
