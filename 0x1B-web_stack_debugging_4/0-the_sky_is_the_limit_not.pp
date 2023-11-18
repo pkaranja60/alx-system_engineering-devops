1ef2532237f4487d9f7122f82a2057233d092bf8
Peter karanja <peterkaranja60@gmail.com>
2023-11-18 21:00:06 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,14 @@
-# Increases the number of requests that nginx can handle.
-# by increasing maximum open files limit for the nginx user.
+# This manuscript increases the amount of traffic an Nginx server can handle
 
-exec { 'set_ulimit_to 5000':
-  command => '/bin/sed -i "s/ULIMIT.*/ULIMIT=\"-n 5000\"/" /etc/default/nginx'
+# Increase the ULIMIT of the default file
+file { 'fix-for-nginx':
+  ensure  => 'file',
+  path    => '/etc/default/nginx',
+  content => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
+}
 
-} -> exec { 'restart nginx':
-  command => '/usr/sbin/service nginx restart',
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
 }
